e1f62bdc2147548f28a51648de3287ea04da618c
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-06-11 11:38:57 +0200

    user data in profile page displayed

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import AllData from "../../enonce/data.json";
 
 export default class Profile extends Component {
-  state = {
-    UserData: AllData.user
-  };
+  state = { user: {} };
+
+  componentDidMount() {
+    this.setState({ user: AllData.user });
+  }
 
   render() {
-    console.log(this.state.UserData);
+    const { user } = this.state;
+    console.log(user);
     return (
       <React.Fragment>
         <h1>Welcome to the Profile Page</h1>
